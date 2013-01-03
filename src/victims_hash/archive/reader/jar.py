084505c53d3ab3d2ef86efc47210230b42978a70
Grant Murphy <gmurphy@redhat.com>
2013-01-03 10:01:44 +1000

    Added functionality to read metadata from JAR files. This works with works with:
    META-INF/MANIFEST.MF
    */pom.properties

--- src/victims_hash/archive/reader/jar.py
+++ src/victims_hash/archive/reader/jar.py
@@ -10,10 +10,59 @@ except ImportError:
     from StringIO import StringIO
 
 
-class JarReader(object):
+def read_manifest(manifest):
+    """
+    Extract the information from the MANIFEST.MF file included with 
+    the JAR file. 
 
-    # TODO: Allow you to specify the things that extract / mine for
-    # information within the JAR file.
+    Parameters: 
+        - `manifest`: File like object to extract the metadata from.
+    """
+
+    metadata = {}
+    for line in manifest.readlines():
+
+        if ':' in line:
+
+            kw, sep, val = line.partition(':') 
+            if kw.endswith('http') and val.startswith('//'):
+                metadata[keyword] += line.rstrip()
+
+            else:
+                keyword = kw
+                metadata[keyword] = val.rstrip()
+
+        else:
+            metadata[keyword] += line.rstrip()
+ 
+    return metadata
+
+
+def read_pom_properties(properties):
+    """
+    Extract meta information from the pom.properties file. This usually
+    includes the artifact id, group id, and version information as per 
+    the maven central repository. 
+
+    Parameters: 
+        - `properties`: File like object to extract the metadata from
+    """
+
+    metadata = {}
+
+    for line in properties: 
+        
+        if line.startswith('#'):
+            continue
+
+        key, sep, value = line.partition('=')
+        metadata[key] = value.rstrip()
+
+    return metadata
+
+
+class JarReader(ArchiveReader):
+
     def readinfo(self, hints={}):
         """
         Extract meta information from the archive/
@@ -25,17 +74,30 @@ class JarReader(object):
         metadata = {}
 
         with zipfile.ZipFile(self.io) as archive:
-            for filename in archive.namelist():
-                pass
 
+            pom_properties = filter(lambda x: x.endswith('pom.properties'),\
+                archive.namelist()).pop(0)
+
+            with archive.open(pom_properties) as properties: 
+                metadata.update(read_pom_properties(properties))
+
+            with archive.open("META-INF/MANIFEST.MF") as manifest:
+                metadata.update(read_manifest(manifest))
+
+        return metadata
+            
     def readfiles(self):
         """
         Read files within the archive and return their content
         """
+
         self.io.seek(0)
         with zipfile.ZipFile(self.io) as archive:
+
             for filename in archive.namelist():
+
                 if filename.endswith(".class"):
+
                     iostr = StringIO(archive.read(filename))
                     # Skip java compiler version.
                     javaclass.read_magic(iostr)
