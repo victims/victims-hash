59352f97ad63c9872f78574f4bf69c93ba22accd
Arun Babu Neelicattu <abn@redhat.com>
2013-05-02 18:34:39 +1000

    Adding file hash to archive fingerprint.

--- src/victims_hash/archive/archive.py
+++ src/victims_hash/archive/archive.py
@@ -3,44 +3,54 @@ import hashlib
 class Archive(object):
 
     def __init__(self, reader, algorithms=["sha512", "sha1"]):
-        
         self.reader = reader
         self.algorithms = algorithms
 
     def metadata(self):
         """
-        Read all metadata associated with this archive 
+        Read all metadata associated with this archive
         """
-        meta = { "meta" : self.reader.readinfo() }
         return {
             "meta" : self.reader.readinfo()
         }
-        
+
+    def filehash(self):
+        try:
+            self.reader.io.seek(0)
+            digest = hashlib.sha512()
+            for buff in iter(self.reader.io.read, b''):
+                digest.update(buff)
+            return digest.hexdigest()
+        except:
+            return ""
+        finally:
+            self.reader.io.seek(0)
+
     def fingerprint(self):
         """
         Create a fingerprint for this archive
         """
+        hashes = {}
 
-        hashes = {} 
-        
-        for algorithm in self.algorithms: 
-            
+        for algorithm in self.algorithms:
+
             files = {}
             combined = hashlib.new(algorithm)
 
-            for (file, content) in self.reader.readfiles():
+            for (filename, content) in self.reader.readfiles():
                 h = hashlib.new(algorithm)
                 h.update(content)
 
-                checksum = h.hexdigest() 
-                files[checksum] = file
+                checksum = h.hexdigest()
+                files[checksum] = filename
                 combined.update(checksum)
 
             hashes[algorithm] = {
-                    "combined"  : combined.hexdigest(), 
+                    "combined"  : combined.hexdigest(),
                     "files"     : files
             }
 
         return {
+            "hash": self.filehash(),
             "hashes": hashes
         }
